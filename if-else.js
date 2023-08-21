94df162795951e8b2c058df63c6c2edb11c77456
Muhammad Adil <141252873+adil273@users.noreply.github.com>
2023-08-21 09:10:09 -0700

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,24 @@
+"use strict";
+var a = 10;
+if (a > 100) {
+    console.log("The value of a is greater then 100");
+}
+else {
+    console.log("The value of a is less then 100");
+}
+var marks = 77;
+if (marks > 80 && marks < 100) {
+    console.log("The grade is A");
+}
+else if (marks > 70 && marks < 80) {
+    console.log("The grade is B");
+}
+else if (marks > 60 && marks < 70) {
+    console.log("The grade is C");
+}
+else if (marks > 50 && marks < 60) {
+    console.log("The grade is D");
+}
+else {
+    console.log("The grade is F");
+}
---
+++ if-else.ts
@@ -0,0 +1,25 @@
+//FIRST EXAMPLE
+var a:number=10;
+if(a>100){
+console.log("The value of a is greater then 100");
+}
+else{
+    console.log("The value of a is less then 100");
+}
+// SECOND EXAMPLE
+var marks:number=77;
+if(marks>80 && marks<100){
+    console.log("The grade is A");
+}
+else if(marks>70 && marks<80){
+    console.log("The grade is B");
+}
+else if(marks>60 && marks<70){
+    console.log("The grade is C");
+}
+else if(marks>50 && marks<60){
+    console.log("The grade is D");
+}
+else{
+    console.log("The grade is F");
+}
